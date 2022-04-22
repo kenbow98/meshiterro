08965fa4c71f15f7133b6ccb3f916d3f37610000
kenbow98 <kenbow.998@gmail.com>
2022-04-22 14:16:52 +0000

    finish 5-6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  #device利用の機能(ユーザー登録、ログイン認証など)が使われる前にconfigure_permitted_parametersメソッドが実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+#configure_permitted_parametersメソッドでは、devise_parameter_sanitizer.permitメソッドを使うことで
+#ユーザー登録(sign_up)の際に、ユーザー名(name)のデータ操作を許可しています。
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
