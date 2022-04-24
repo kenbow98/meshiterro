68f4e4bdbe6f9cab2b353a4cfa714382d174114e
kenbow98 <kenbow.998@gmail.com>
2022-04-24 20:29:43 +0000

    finish 5-16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,22 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+   def update
+    #ユーザーの取得
+    @user = User.find(params[:id])
+    #ユーザーのアップデート
+    @user.update(user_params)
+    # ユーザーの詳細ページへのパス
+    redirect_to user_path(@user)
+   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
