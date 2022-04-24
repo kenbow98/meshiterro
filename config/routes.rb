42102917e6e74ca961f215834645ae54d6c0da5b
kenbow98 <kenbow.998@gmail.com>
2022-04-24 14:32:29 +0000

    finish 5-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
-
-
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-
-
   # 名前付きルートとは、文字通りルーティング自体に名前をつけておくこと
   #as:オプションを追加することで設定ができる
   #ここでは、「'lists#show'の設定を、listとして利用できる」の意味（asは、英語で「～として」の意味）。
   get 'homes/about' => 'homes#about',as:'about'
 
-
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit]
 
   end
